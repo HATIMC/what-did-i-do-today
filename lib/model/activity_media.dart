69d1bd9edd3fecb9ba61bd7ee2fbc80af972aae2
Hatim Chathiwala <hatim.cathiwala@gmail.com>
2025-08-10 15:49:33 +0530

    add activity sheet

--- lib/model/activity_media.dart
+++ lib/model/activity_media.dart
@@ -1,9 +1,32 @@
+import 'dart:convert';
+
 class ActivityMedia {
   final String mediaId;
   final String mediaPath;
 
-  ActivityMedia({
-    required this.mediaId,
-    required this.mediaPath,
-  });
+  ActivityMedia({required this.mediaId, required this.mediaPath});
+
+  // JSON serialization methods
+  Map<String, dynamic> toJson() {
+    return {'mediaId': mediaId, 'mediaPath': mediaPath};
+  }
+
+  String toRawJson() => json.encode(toJson());
+
+  factory ActivityMedia.fromJson(Map<String, dynamic> json) {
+    return ActivityMedia(
+      mediaId: json['mediaId'] ?? '',
+      mediaPath: json['mediaPath'] ?? '',
+    );
+  }
+
+  factory ActivityMedia.fromRawJson(String str) =>
+      ActivityMedia.fromJson(json.decode(str));
+
+  ActivityMedia copyWith({String? mediaId, String? mediaPath}) {
+    return ActivityMedia(
+      mediaId: mediaId ?? this.mediaId,
+      mediaPath: mediaPath ?? this.mediaPath,
+    );
+  }
 }
